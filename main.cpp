17f62aca87c4c6181c12adcc5a984faff3a5497a
Ryan Bibby <r.bibby@gmail.com>
2024-06-23 21:23:48 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <cstdlib>
+
+#include "libraries/pico_display_2/pico_display_2.hpp"
+#include "drivers/st7789/st7789.hpp"
+#include "libraries/pico_graphics/pico_graphics.hpp"
+
+using namespace pimoroni;
+
+ST7789 st7789(320, 240, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
+PicoGraphics_PenRGB332 graphics(st7789.width, st7789.height, nullptr);
+
+int main() {
+  st7789.set_backlight(255);
+
+  struct pt {
+    float      x;
+    float      y;
+    uint8_t    r;
+    float     dx;
+    float     dy;
+    uint16_t pen;
+  };
+
+  std::vector<pt> shapes;
+  for(int i = 0; i < 100; i++) {
+    pt shape;
+    shape.x = rand() % graphics.bounds.w;
+    shape.y = rand() % graphics.bounds.h;
+    shape.r = (rand() % 10) + 3;
+    shape.dx = float(rand() % 255) / 64.0f;
+    shape.dy = float(rand() % 255) / 64.0f;
+    shape.pen = graphics.create_pen(rand() % 255, rand() % 255, rand() % 255);
+    shapes.push_back(shape);
+  }
+
+  Point text_location(0, 0);
+
+  Pen BG = graphics.create_pen(120, 40, 60);
+  Pen WHITE = graphics.create_pen(255, 255, 255);
+
+  while(true) {
+
+    graphics.set_pen(BG);
+    graphics.clear();
+
+    for(auto &shape : shapes) {
+      shape.x += shape.dx;
+      shape.y += shape.dy;
+      if((shape.x - shape.r) < 0) {
+        shape.dx *= -1;
+        shape.x = shape.r;
+      }
+      if((shape.x + shape.r) >= graphics.bounds.w) {
+        shape.dx *= -1;
+        shape.x = graphics.bounds.w - shape.r;
+      }
+      if((shape.y - shape.r) < 0) {
+        shape.dy *= -1;
+        shape.y = shape.r;
+      }
+      if((shape.y + shape.r) >= graphics.bounds.h) {
+        shape.dy *= -1;
+        shape.y = graphics.bounds.h - shape.r;
+      }
+
+      graphics.set_pen(shape.pen);
+      graphics.circle(Point(shape.x, shape.y), shape.r);
+
+    }
+
+
+
+
+    graphics.set_pen(WHITE);
+    graphics.text("Hello World", text_location, 320);
+
+    // update screen
+    st7789.update(&graphics);
+  }
+
+    return 0;
+}
