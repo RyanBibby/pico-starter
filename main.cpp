9e23218de4911ce4a833453795b3b6e7870e3df7
Ryan Bibby <r.bibby@gmail.com>
2024-06-26 19:13:57 +0100

    Baseline

--- main.cpp
+++ main.cpp
@@ -1,85 +1,24 @@
-#include <string.h>
-#include <math.h>
-#include <vector>
-#include <cstdlib>
-
-#include "libraries/pico_display_2/pico_display_2.hpp"
-#include "drivers/st7789/st7789.hpp"
-#include "libraries/pico_graphics/pico_graphics.hpp"
-
-using namespace pimoroni;
-
-ST7789 st7789(320, 240, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
-PicoGraphics_PenRGB332 graphics(st7789.width, st7789.height, nullptr);
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
-  st7789.set_backlight(255);
 
-  struct pt {
-    float      x;
-    float      y;
-    uint8_t    r;
-    float     dx;
-    float     dy;
-    uint16_t pen;
-  };
+    const uint LED_PIN = 7;
 
-  std::vector<pt> shapes;
-  for(int i = 0; i < 100; i++) {
-    pt shape;
-    shape.x = rand() % graphics.bounds.w;
-    shape.y = rand() % graphics.bounds.h;
-    shape.r = (rand() % 10) + 3;
-    shape.dx = float(rand() % 255) / 64.0f;
-    shape.dy = float(rand() % 255) / 64.0f;
-    shape.pen = graphics.create_pen(rand() % 255, rand() % 255, rand() % 255);
-    shapes.push_back(shape);
-  }
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    stdio_init_all();
 
-  Point text_location(0, 0);
-
-  Pen BG = graphics.create_pen(120, 40, 60);
-  Pen WHITE = graphics.create_pen(255, 255, 255);
-
-  while(true) {
-
-    graphics.set_pen(BG);
-    graphics.clear();
-
-    for(auto &shape : shapes) {
-      shape.x += shape.dx;
-      shape.y += shape.dy;
-      if((shape.x - shape.r) < 0) {
-        shape.dx *= -1;
-        shape.x = shape.r;
-      }
-      if((shape.x + shape.r) >= graphics.bounds.w) {
-        shape.dx *= -1;
-        shape.x = graphics.bounds.w - shape.r;
-      }
-      if((shape.y - shape.r) < 0) {
-        shape.dy *= -1;
-        shape.y = shape.r;
-      }
-      if((shape.y + shape.r) >= graphics.bounds.h) {
-        shape.dy *= -1;
-        shape.y = graphics.bounds.h - shape.r;
-      }
-
-      graphics.set_pen(shape.pen);
-      graphics.circle(Point(shape.x, shape.y), shape.r);
-
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
     }
-
-
-
-
-    graphics.set_pen(WHITE);
-    graphics.text("Hello World", text_location, 320);
-
-    // update screen
-    st7789.update(&graphics);
-  }
-
-    return 0;
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+         gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+         gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+    }
 }
